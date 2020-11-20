e13c253a42db66c0ca3f64abab2996ebae663691
mopomi <mrooooo.33@gmail.com>
2020-11-20 15:17:49 +0900

    second commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
 before_action :move_to_index, only: [ :edit ]
-before_action :authenticate_user!, except: [:index, :show, :new, :create ]
+before_action :authenticate_user!, except: [:index, :show ]
   
   def index
     @prototypes = Prototype.all
